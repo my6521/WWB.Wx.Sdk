7a96d92a604fcf039dbfc4f5758dfb577e7b96d6
王 <22379099@qq.com>
2021-11-25 18:56:44 +0800

    1.0.4

---
+++ src/WWB.Wx.Sdk/Apis/QrCode/Dtos/GetWxaCodeRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WWB.Wx.Sdk.Apis.QrCode.Dtos
+{
+    public class GetWxaCodeRequest
+    {
+        public string path { get; set; }
+        public int? width { get; set; }
+        public bool auto_color { get; set; }
+        public object line_color { get; set; }
+        public bool is_hyaline { get; set; }
+    }
+}
--- src/WWB.Wx.Sdk/Apis/QrCode/IQrCodeApi.cs
+++ src/WWB.Wx.Sdk/Apis/QrCode/IQrCodeApi.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using WebApiClientCore.Attributes;
 using WWB.Wx.Sdk.Apis.QrCode.Dtos;
@@ -9,11 +9,20 @@ namespace WWB.Wx.Sdk.Apis.QrCode
     public interface IQrCodeApi : IWxApiBase
     {
         /// <summary>
-        /// 小程序一物一码
+        /// 接口 B: 获取小程序码，适用于需要的码数量极多的业务场景
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
         [HttpPost("getwxacodeunlimit")]
-        Task<Stream> GetwxacodeUnlimit([PathQuery] string access_token, [JsonNetContent] GetWxacodeUnlimitRequest request);
+        Task<HttpResponseMessage> GetwxacodeUnlimit([PathQuery] string access_token, [JsonNetContent] GetWxacodeUnlimitRequest request);
+
+        /// <summary>
+        /// 接口 A: 获取小程序码，适用于需要的码数量较少的业务场景
+        /// </summary>
+        /// <param name="access_token"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost("getwxacode")]
+        Task<HttpResponseMessage> Getwxacode([PathQuery] string access_token, [JsonNetContent] GetWxaCodeRequest request);
     }
 }
